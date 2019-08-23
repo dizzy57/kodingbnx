3d5cd36a4e8d4e8948c891dfed2a0f7578cbba14
Alexey Preobrazhenskiy <dizzy57@gmail.com>
2019-08-23 22:03:30 +0200

    Add constraint to solution, make fields non-editable

--- coding_tasks/models.py
+++ coding_tasks/models.py
@@ -5,10 +5,13 @@ from django.db import models
 class Task(models.Model):
     name = models.CharField(max_length=255)
     url = models.URLField(max_length=255)
-    date = models.DateField(unique=True)
+    date = models.DateField(editable=False, unique=True)
 
 
 class Solution(models.Model):
-    task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    class Meta:
+        unique_together = ["task", "user"]
+
+    task = models.ForeignKey(Task, editable=False, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, editable=False, on_delete=models.PROTECT)
     url = models.URLField(max_length=255)
