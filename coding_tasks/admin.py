ae76ea26d63161494f208aeca0af5f385d13fda1
Alexey Preobrazhenskiy <dizzy57@gmail.com>
2019-08-23 20:26:11 +0200

    Add initial models

--- coding_tasks/admin.py
+++ coding_tasks/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from coding_tasks.models import Solution, Task
+
+admin.site.register((Solution, Task))
--- coding_tasks/models.py
+++ coding_tasks/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.URLField(max_length=255)
+    date = models.DateField(unique=True)
+
+
+class Solution(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    url = models.URLField(max_length=255)
