716f40cd52b8acc07cd5e19ea7ee1a53b1fd43ec
Alexey Preobrazhenskiy <dizzy57@gmail.com>
2019-09-11 19:30:48 +0200

    Implement Task.today

--- coding_tasks/models.py
+++ coding_tasks/models.py
@@ -1,11 +1,26 @@
+import datetime
+
+import pytz
 from django.contrib.auth.models import User
 from django.db import models
+
+TZ = pytz.timezone("Europe/Amsterdam")
 
 
 class Task(models.Model):
     name = models.CharField(max_length=255)
     url = models.URLField(max_length=255)
     date = models.DateField(editable=False, unique=True)
+
+    @classmethod
+    def today(cls, date=None):
+        if date is None:
+            now = datetime.datetime.now(TZ)
+            date = now.date()
+        try:
+            return cls.objects.get(date=date)
+        except cls.DoesNotExist:
+            return None
 
 
 class Solution(models.Model):
@@ -15,3 +30,4 @@ class Solution(models.Model):
     task = models.ForeignKey(Task, editable=False, on_delete=models.CASCADE)
     user = models.ForeignKey(User, editable=False, on_delete=models.PROTECT)
     url = models.URLField(max_length=255)
+    submitted_at = models.DateTimeField(auto_now=True)
--- coding_tasks/views.py
+++ coding_tasks/views.py
@@ -1,16 +1,21 @@
-from django.views.generic import TemplateView, FormView
+from django.urls import reverse_lazy
+from django.views.generic import FormView, TemplateView
 
 from coding_tasks.forms import SubmitSolutionForm
+from coding_tasks.models import Solution, Task
 
 
 class SubmitView(FormView):
     template_name = "submit.html"
     form_class = SubmitSolutionForm
-    success_url = "/solutions"
+    success_url = reverse_lazy("solutions")
 
     def form_valid(self, form):
-        user = self.request.user
-        task = ...  # Tasks.today
+        task = Task.today()
+        if task:
+            Solution.objects.get_or_create(
+                task=task, user=self.request.user, defaults={"url": form.url}
+            )
         return super().form_valid(form)
 
 
