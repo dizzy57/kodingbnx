d9d088a4cb18ab1bdf94614f74c9d65777dc9f40
Alexey Preobrazhenskiy <dizzy57@gmail.com>
2019-11-04 19:47:08 +0100

    Add secret suffix to admin URL

--- kodingbnx/urls.py
+++ kodingbnx/urls.py
@@ -13,13 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
+ADMIN_SUFFIX = os.environ["DJANGO_ADMIN_SUFFIX"]
+
 urlpatterns = [
     path("", include("coding_tasks.urls")),
-    path("admin/", admin.site.urls),
+    path(f"admin{ADMIN_SUFFIX}/", admin.site.urls),
     path("bot_api/", include("telegram_bot.urls")),
 ]
 
