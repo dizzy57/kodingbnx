1649979a23e8ea05213403e2118411b15bff55bb
Alexey Preobrazhenskiy <dizzy57@gmail.com>
2019-11-07 20:51:53 +0100

    Rework bot and cron interfaces for clarity

--- telegram_bot/telegram_api.py
+++ telegram_bot/telegram_api.py
@@ -1,23 +1,26 @@
+import datetime
 import os
 
 import requests
 
+from coding_tasks import task_schedule
+from coding_tasks.models import Solution, Task
+
 BOT_API_KEY = os.environ["TELEGRAM_BOT_API_KEY"]
 CHAT_ID = int(os.environ["TELEGRAM_CHAT_ID"])
-
 URL_BASE = f"https://api.telegram.org/bot{BOT_API_KEY}/"
+
+SITE_URL = "https://kodingbnx.pythonanywhere.com/"
+NO_TOMORROW_TASK_MESSAGE = "@asizikov @dizzy57 No task for tomorrow"
+FIRST_DAY_OF_MONTH_MESSAGE = "@dizzy57 Refresh the hosting"
 
 
 class TelegramApi:
     def __init__(self):
         self.session = requests.session()
 
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def close(self):
         self.session.close()
-        return False
 
     def _call(self, endpoint, data):
         res = self.session.post(URL_BASE + endpoint, json=data)
@@ -35,3 +38,52 @@ class TelegramApi:
             "disable_notification": True,
         }
         return self._call("pinChatMessage", data)
+
+
+class TelegramBot:
+    def __init__(self):
+        self.api = TelegramApi()
+        self.today = task_schedule.today()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.api.close()
+        return False
+
+    def send_and_pin_task_for_today(self):
+        try:
+            task = Task.objects.get(date=self.today)
+        except Task.DoesNotExist:
+            self.api.send_message("No task for today :(")
+            return
+
+        m = self.api.send_message("\n".join((task.name, task.url, SITE_URL)))
+        self.api.pin_message(m["result"]["message_id"])
+
+    def send_solutions_for_today(self):
+        solutions = list(
+            Solution.objects.filter(task__date=self.today)
+            .order_by("user__first_name")
+            .select_related("user")
+        )
+
+        text = f"Solutions {self.today:%d/%m}:\n"
+        if solutions:
+            text += "\n".join(
+                f"{solution.user.first_name} {solution.url}" for solution in solutions
+            )
+        else:
+            text += "None"
+
+        self.api.send_message(text)
+
+    def notify_if_no_tasks_for_tomorrow(self):
+        tomorrow = self.today + datetime.timedelta(days=1)
+        if not Task.objects.filter(date=tomorrow):
+            self.api.send_message(NO_TOMORROW_TASK_MESSAGE)
+
+    def notify_if_first_day_of_month(self):
+        if self.today.day == 1:
+            self.api.send_message(FIRST_DAY_OF_MONTH_MESSAGE)
--- telegram_bot/views.py
+++ telegram_bot/views.py
@@ -1,19 +1,14 @@
-import datetime
 import functools
 import os
 
 from django.http import HttpResponse
 
-from coding_tasks import task_schedule
-from coding_tasks.models import Solution, Task
-from telegram_bot.telegram_api import TelegramApi
+from telegram_bot.telegram_api import TelegramBot
 
 CRON_API_KEY = os.environ["CRON_API_KEY"]
-SITE_URL = "https://kodingbnx.pythonanywhere.com/"
-NO_TOMORROW_TASK_MESSAGE = "@asizikov @dizzy57 No task for tomorrow"
 
 
-def check_api_key(f):
+def cronjob_api_call(f):
     @functools.wraps(f)
     def wrapped(request):
         key = request.GET.get("api_key")
@@ -25,44 +20,16 @@ def check_api_key(f):
     return wrapped
 
 
-@check_api_key
+@cronjob_api_call
 def morning_send_task():
-    with TelegramApi() as api:
-        today = task_schedule.today()
-        try:
-            task = Task.objects.get(date=today)
-        except Task.DoesNotExist:
-            api.send_message("No task for today :(")
-            return
-
-        m = api.send_message("\n".join((task.name, task.url, SITE_URL)))
-        api.pin_message(m["result"]["message_id"])
-        notify_if_no_task_for_tomorrow(api)
+    with TelegramBot() as bot:
+        bot.send_and_pin_task_for_today()
+        bot.notify_if_no_tasks_for_tomorrow()
+        bot.notify_if_first_day_of_month()
 
 
-@check_api_key
+@cronjob_api_call
 def evening_send_solutions():
-    today = task_schedule.today()
-    solutions = list(
-        Solution.objects.filter(task__date=today)
-        .order_by("user__first_name")
-        .select_related("user")
-    )
-
-    text = f"Solutions {today:%d/%m}:\n"
-    if solutions:
-        text += "\n".join(
-            f"{solution.user.first_name} {solution.url}" for solution in solutions
-        )
-    else:
-        text += "None"
-
-    with TelegramApi() as api:
-        api.send_message(text)
-        notify_if_no_task_for_tomorrow(api)
-
-
-def notify_if_no_task_for_tomorrow(api):
-    tomorrow = task_schedule.today() + datetime.timedelta(days=1)
-    if not Task.objects.filter(date=tomorrow):
-        api.send_message(NO_TOMORROW_TASK_MESSAGE)
+    with TelegramBot() as bot:
+        bot.send_solutions_for_today()
+        bot.notify_if_no_tasks_for_tomorrow()
