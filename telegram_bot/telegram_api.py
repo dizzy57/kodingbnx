4836c65c203c65ab7067c392a20db705208d552f
Alexey Preobrazhenskiy <dizzy57@gmail.com>
2019-11-28 20:56:39 +0100

    Refactor URLs in bot messages

--- telegram_bot/telegram_api.py
+++ telegram_bot/telegram_api.py
@@ -2,6 +2,7 @@ import datetime
 import os
 
 import requests
+from django.urls import reverse
 
 from coding_tasks import task_schedule
 from coding_tasks.models import Solution, Task
@@ -10,7 +11,7 @@ BOT_API_KEY = os.environ["TELEGRAM_BOT_API_KEY"]
 CHAT_ID = int(os.environ["TELEGRAM_CHAT_ID"])
 URL_BASE = f"https://api.telegram.org/bot{BOT_API_KEY}/"
 
-SITE_URL = "https://kodingbnx.pythonanywhere.com/"
+SITE_URL = "https://kodingbnx.pythonanywhere.com"
 STAFF_USERS = "@asizikov @dizzy57"
 NO_TASK_FOR_TODAY_MESSAGE = f"{STAFF_USERS} No task for today :("
 NO_TASK_FOR_TOMORROW_MESSAGE = f"{STAFF_USERS} No task for tomorrow"
@@ -60,11 +61,12 @@ class TelegramBot:
             self.api.send_message(NO_TASK_FOR_TODAY_MESSAGE)
             return
 
-        m = self.api.send_message("\n".join((task.name, task.url, SITE_URL)))
+        m = self.api.send_message("\n".join((task.name, task.url, SITE_URL + "/")))
         self.api.pin_message(m["result"]["message_id"])
 
     def send_solutions_for_today(self):
-        text = f"Solutions:\n{SITE_URL}solutions/{self.today:%Y-%m-%d}"
+        today_solutions_url = reverse("solutions_day", kwargs={"date": self.today})
+        text = f"Solutions:\n{SITE_URL}{today_solutions_url}"
         self.api.send_message(text)
 
     def notify_if_no_tasks_for_tomorrow(self):
@@ -73,5 +75,6 @@ class TelegramBot:
             self.api.send_message(NO_TASK_FOR_TOMORROW_MESSAGE)
 
     def notify_additional_solution(self, solution: Solution):
-        text = f"New solution by {solution.user.first_name}: {SITE_URL}solutions/{self.today:%Y-%m-%d}#u{solution.user.id}"
+        today_solutions_url = reverse("solutions_day", kwargs={"date": self.today})
+        text = f"New solution by {solution.user.get_short_name()} ({solution.language}): {SITE_URL}{today_solutions_url}#u{solution.user.id}"
         self.api.send_message(text)
