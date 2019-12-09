eeef375a331781c88c359a6bd2db4bb1d3d18181
Alexey Preobrazhenskiy <dizzy57@gmail.com>
2019-12-09 20:28:01 +0100

    Living on the edge

--- coding_tasks/models.py
+++ coding_tasks/models.py
@@ -1,21 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-SOLUTION_LANGUAGE_CHOICES = [
-    ("c", "C"),
-    ("cpp", "C++"),
-    ("csharp", "C#"),
-    ("go", "Go"),
-    ("java", "Java"),
-    ("js", "JavaScript"),
-    ("kotlin", "Kotlin"),
-    ("python", "Python"),
-    ("ruby", "Ruby"),
-    ("rust", "Rust"),
-    ("scala", "Scala"),
-    ("text", "Text only"),
-]
-
 
 class Task(models.Model):
     class Meta:
@@ -33,10 +18,24 @@ class Solution(models.Model):
     class Meta:
         unique_together = ["task", "user"]
 
+    class SolutionLanguage(models.TextChoices):
+        C = "c", "C"
+        CPP = "cpp", "C++"
+        CSHARP = "csharp", "C#"
+        GO = "go", "Go"
+        JAVA = "java", "Java"
+        JAVASCRIPT = "js", "JavaScript"
+        KOTLIN = "kotlin", "Kotlin"
+        PYTHON = "python", "Python"
+        RUBY = "ruby", "Ruby"
+        RUST = "rust", "Rust"
+        SCALA = "scala", "Scala"
+        TEXT = "text", "Text only"
+
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     code = models.TextField()
     language = models.CharField(
-        max_length=6, choices=SOLUTION_LANGUAGE_CHOICES, default="text"
+        max_length=6, choices=SolutionLanguage.choices, default=SolutionLanguage.TEXT
     )
     submitted_at = models.DateTimeField(auto_now=True)
