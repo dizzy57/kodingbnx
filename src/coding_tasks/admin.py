e4fbc1d2c7a6d397bdc0fb47a01e0d0108f70361
Alexey Preobrazhenskiy <dizzy57@gmail.com>
2020-02-07 19:27:39 +0100

    Show profiles in admin

--- src/coding_tasks/admin.py
+++ src/coding_tasks/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from coding_tasks.models import Solution, Task
+from coding_tasks.models import Profile, Solution, Task
 
 
 @admin.register(Task)
@@ -12,6 +12,11 @@ class TaskAdmin(admin.ModelAdmin):
 
 @admin.register(Solution)
 class SolutionAdmin(admin.ModelAdmin):
-    list_display = ["user", "task", "language"]
+    list_display = ["task", "user", "language"]
     ordering = ["-submitted_at"]
     list_filter = ["task__date"]
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["user", "away_until"]
