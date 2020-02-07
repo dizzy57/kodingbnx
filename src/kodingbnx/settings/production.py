fdeb23a6bb8758d73bae37b4214b41c26f57e076
Alexey Preobrazhenskiy <dizzy57@gmail.com>
2020-02-07 21:59:43 +0100

    Use mysql 5.7 both in development and production

--- src/kodingbnx/settings/production.py
+++ src/kodingbnx/settings/production.py
@@ -4,8 +4,6 @@ ALLOWED_HOSTS = ["kodingbnx.pythonanywhere.com"]
 
 INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
-DATABASES["default"]["OPTIONS"]["sql_mode"] = "STRICT_TRANS_TABLES"
-
 STATIC_ROOT = "/home/kodingbnx/static"
 
 SESSION_COOKIE_AGE = 365 * 24 * 60 * 60  # 365 days in seconds
